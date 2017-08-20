7b76e8f4b0ace4658130b4191256048371d66e6f
mkryst <michkryst@gmail.com>
2017-08-21 00:44:40 +0200

    Initial commit

---
+++ airquality.py
@@ -0,0 +1,7 @@
+#(1.) Download the dataset from https://archive.ics.uci.edu/ml/datasets/Air+quality and save .csv file into /data
+
+#(2.) Load the dataset into pandas data frame
+
+#(3.) Create a timeseries data frame
+
+#(4.) Remove all the values of NO2(GT) greater than 100 from march 2005 till june 2005
---
+++ orgate.py
@@ -0,0 +1,5 @@
+#(5.) Implement logical OR gate using neural network and scikit learn library
+# - Reading:
+# -- http://www.mind.ilstu.edu/curriculum/artificial_neural_net/neural_nets_for_logical_functions.php
+# -- http://www.cs.bham.ac.uk/~jxb/NN/l3.pdf
+# Note: You should create your own training and test datasets

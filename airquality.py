94e80d6c61117c0f38801813bf6ae1fc6dbb98b0
abhinandante <abhinandan@thirdeyecss.com>
2017-08-25 00:10:38 +0530

    add dataframe filtering

--- airquality.py
+++ airquality.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 #(2.) Load the dataset into pandas data frame
-df = pd.read_csv('/home/abhinandan/DataScience/AirQualityUCI/AirQualityUCI.csv',sep=";")
+df = pd.read_csv('data/AirQualityUCI.csv',sep=";")
 
 #(3.) Create a timeseries data frame
 df['Date'] = pd.to_datetime(df['Date'])
@@ -12,3 +12,4 @@ df.index = df['Date']
 
 #(4.) Remove all the values of NO2(GT) greater than 100 from march 2005 till june 2005
 df = df[(df['Date'] >='2005-03') & (df['Date'] <= '2005-06') & (df['NO2(GT)'] > 100)]
+print(df)

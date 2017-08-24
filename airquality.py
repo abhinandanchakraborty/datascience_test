cb260484f0ec41367ec209bb79d8ab7dc760543c
abhinandante <abhinandan@thirdeyecss.com>
2017-08-25 00:30:41 +0530

    add dataframe time series

--- airquality.py
+++ airquality.py
@@ -8,8 +8,10 @@ df = pd.read_csv('data/AirQualityUCI.csv',sep=";")
 #(3.) Create a timeseries data frame
 df['Date'] = pd.to_datetime(df['Date'])
 df.index = df['Date']
-#del df['Date']
+del df['Date']
 
 #(4.) Remove all the values of NO2(GT) greater than 100 from march 2005 till june 2005
-df = df[(df['Date'] >='2005-03') & (df['Date'] <= '2005-06') & (df['NO2(GT)'] > 100)]
+#df = df[(df['Date'] >='2005-03') & (df['Date'] <= '2005-06') & (df['NO2(GT)'] > 100)]
+df = df['2005-03':'2005-06']
+df = df[(df['NO2(GT)'] > 100)]
 print(df)
